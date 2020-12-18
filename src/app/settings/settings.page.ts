442c90ca2127a72f2c9808c392543f504346336f
Matthieu Mota <matthieumota@gmail.com>
2020-12-18 11:42:33 +0100

    Spinner, message du formulaire

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -13,13 +13,22 @@ export class SettingsPage implements OnInit {
     city: '',
     darkMode: false,
   };
+  // Booléen pour savoir si on affiche une icône de chargement
+  saving = false;
+  // Booléen pour savoir si on doit afficher un message
+  success = false;
 
   constructor(private storage: Storage) { }
 
   // Quand on arrive sur la page settings, on va récupèrer les données
   // du téléphone
   ngOnInit() {
-    this.storage.get('settings').then(settings => this.settings = settings);
+    this.storage.get('settings').then(settings => {
+      // On vérifie bien qu'il y a des settings dans le storage
+      if (settings !== null) {
+        this.settings = settings;
+      }
+    });
   }
 
   // Active ou non le dark mode sur l'application
@@ -34,7 +43,12 @@ export class SettingsPage implements OnInit {
     // On va utiliser le service storage pour stocker les settings sur
     // le téléphone
     console.log(this.settings);
-    this.storage.set('settings', this.settings);
+    this.saving = true; // Chargement en cours...
+    this.storage.set('settings', this.settings).then(() => {
+      setTimeout(() => this.saving = false, 2000); // Chargement terminé, promesse tenue
+      setTimeout(() => this.success = true, 2000);
+      setTimeout(() => this.success = false, 10000); // On cache le texte au bout de 10 secondes
+    });
   }
 
   // Suppression des données sur le téléphone
