2177a09dea2882c19be0b2ab5372e8d4e84c9eb9
Matthieu Mota <matthieumota@gmail.com>
2020-12-18 10:31:41 +0100

    Installation du storage

--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +7,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  // Objet qui va stocker toutes les informations
+  settings = {
+    username: '',
+    city: '',
+    darkMode: false,
+  };
 
-  constructor() { }
+  constructor(private storage: Storage) { }
 
+  // Quand on arrive sur la page settings, on va récupèrer les données
+  // du téléphone
   ngOnInit() {
+    this.storage.get('settings').then(settings => this.settings = settings);
   }
 
   // Active ou non le dark mode sur l'application
@@ -17,6 +27,14 @@ export class SettingsPage implements OnInit {
     console.log('dark mode');
     // On ajoute ou on supprime la classe dark de body
     document.body.classList.toggle('dark');
+  }
+
+  // Enregistrer les données du formulaire dans le téléphone
+  save() {
+    // On va utiliser le service storage pour stocker les settings sur
+    // le téléphone
+    console.log(this.settings);
+    this.storage.set('settings', this.settings);
   }
 
 }
